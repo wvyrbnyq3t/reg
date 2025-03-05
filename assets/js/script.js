08374807545b2c5f29b96ca426bc0d0071ba21bd
SotaDesktop <kansota1125@gmail.com>
2025-03-05 17:48:23 +0900

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,38 @@
+// 履歴
+const history = new Array();
+
+const generateMatchingString = (prefix, bit) => {
+  const characters =
+    "abcdedfghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  let matchingString = `${prefix || ""}`;
+
+  for (let i = 0; i < parseInt(bit); i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    matchingString += characters[randomIndex];
+  }
+
+  const isExist = history.includes(matchingString);
+
+  if (isExist) {
+    return generateMatchingString(prefix, bit);
+  }
+
+  history.push(matchingString);
+  console.log(history);
+  return matchingString;
+};
+
+const regResult = document.getElementById("regResult");
+const btnGenerate = document.getElementById("btnGenerate");
+const btnClip = document.getElementById("btnClip");
+
+btnGenerate.addEventListener("click", () => {
+  const prefix = document.getElementById("prefix").value;
+  const numBit = document.getElementById("bit").value;
+  regResult.innerText = generateMatchingString(prefix, numBit);
+});
+
+btnClip.addEventListener("click", () => {
+  navigator.clipboard.writeText(regResult.innerText);
+  alert("Copied to clipboard!");
+});
